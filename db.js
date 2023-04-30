598d0418f1e2ec7d1324080c8b3517a8d7a60dca
Rotem Horovitz <lurxie@gmail.com>
2023-04-30 22:58:49 +0300

    .

--- db.js
+++ db.js
@@ -5,7 +5,10 @@ const serviceAccount = {
   type: 'service_account',
   project_id: 'professor-game',
   private_key_id: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(
+    /\\n/g,
+    '\n'
+  ),
   client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
   client_id: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID,
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
